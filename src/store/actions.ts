269ac9bea7308e4b3dc4ae395e82ebd098c3bdc3
junaidfarooqui <junaidfarooqui61@gmail.com>
2019-02-05 22:17:03 +0100

    set books methid

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -3,3 +3,6 @@ const startSearch = () => ({ type: START_SEARCH })
 
 export const SET_REQUEST_ERROR = 'SET_REQUEST_ERROR'
 const setRequestError = () => ({ type: SET_REQUEST_ERROR, error })
+
+export const SET_BOOKS = 'SET_BOOKS'
+const setBooks = () => ({ type: SET_BOOKS, items })
