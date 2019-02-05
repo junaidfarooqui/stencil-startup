ec64c144f5a2e4f37a44c57241169d5d0fc3c85c
junaidfarooqui <junaidfarooqui61@gmail.com>
2019-02-05 23:17:19 +0100

    function added and API included

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -2,13 +2,29 @@ export const START_SEARCH = 'START_SEARCH';
 const startSearch = () => ({ type: START_SEARCH });
 
 export const SET_REQUEST_ERROR = 'SET_REQUEST_ERROR';
-const setRequestError = () => ({ type: SET_REQUEST_ERROR, error });
+const setRequestError = (error) => ({ type: SET_REQUEST_ERROR, error });
 
 export const SET_BOOKS = 'SET_BOOKS';
-const setBooks = () => ({ type: SET_BOOKS, items });
+const setBooks = (items) => ({ type: SET_BOOKS, items });
 
 export function searchBooks(query) {
     return dispatch => {
         dispatch(startSearch())
+
+        const serializedQuery = query.split(' ').join('+');
+        const url = 'https://www.googleapis.com/books/v1/volumes?q=${serializedQuery}&maxResults=40';
+
+        fetch(url)
+            .then(
+                res => res.json(),
+                () => dispatch(setRequestError('An unexpected problem occurred'))
+            )
+            .then(data => {
+                if (!data.items) {
+                    dispatch(setRequestError('No results...'))
+                } else {
+                    dispatch(setBooks(data.items))
+                }
+            })
     }
 }
