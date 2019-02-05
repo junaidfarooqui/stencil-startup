97bc047c8e0785b4087a37316917b9cc69ef5e7a
junaidfarooqui <junaidfarooqui61@gmail.com>
2019-02-05 22:18:59 +0100

    dispatch start serch

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,8 +1,14 @@
-export const START_SEARCH = 'START_SEARCH'
-const startSearch = () => ({ type: START_SEARCH })
+export const START_SEARCH = 'START_SEARCH';
+const startSearch = () => ({ type: START_SEARCH });
 
-export const SET_REQUEST_ERROR = 'SET_REQUEST_ERROR'
-const setRequestError = () => ({ type: SET_REQUEST_ERROR, error })
+export const SET_REQUEST_ERROR = 'SET_REQUEST_ERROR';
+const setRequestError = () => ({ type: SET_REQUEST_ERROR, error });
 
-export const SET_BOOKS = 'SET_BOOKS'
-const setBooks = () => ({ type: SET_BOOKS, items })
+export const SET_BOOKS = 'SET_BOOKS';
+const setBooks = () => ({ type: SET_BOOKS, items });
+
+export function searchBooks(query) {
+    return dispatch => {
+        dispatch(startSearch())
+    }
+}
