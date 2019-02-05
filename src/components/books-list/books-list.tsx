48a031d33d05f39a94827e2a085a3743d8aa7fda
junaidfarooqui <junaidfarooqui61@gmail.com>
2019-02-05 22:02:14 +0100

    book list update with a loop

--- src/components/books-list/books-list.tsx
+++ src/components/books-list/books-list.tsx
@@ -6,14 +6,30 @@ import {Component, Prop} from "@stencil/core";
 })
 
 export class BooksList {
-    @Prop() books: Array<any> = []
+    @Prop() books: Array<any> = [];
 
     render() {
-        const { book } = this
+        const { books } = this;
 
         return (
             <ul>
-                
+                {books.map(book => {
+                    const {previewLink, title, imageLinks, authors} = book.volumeInfo;
+                    const thumbnail = imageLinks ? imageLinks.thumbnail : '';
+                    const _authors = authors ? authors.join(', ') : 'unknown';
+
+                    return (
+                        <li>
+                            <a href={previewLink} target='__blank'></a>
+                            <img src={thumbnail} class='cover'/>
+                            <div class='info'>
+                                <div class='title'>{title}</div>
+                                <div class='authors'>{_authors}</div>
+                            </div>
+                        </li>
+                    )
+                })
+                }
             </ul>
         )
     }
