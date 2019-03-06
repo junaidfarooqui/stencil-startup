578755668992b0525d356f61c29f4588cfef0ea7
junaidfarooqui <junaidfarooqui61@gmail.com>
2019-03-06 22:12:06 +0100

    styling and classes added

--- src/components/components-list/components-list.tsx
+++ src/components/components-list/components-list.tsx
@@ -10,9 +10,11 @@ export class ComponentsList {
     render () {
         return (
             <div class="container">
-                <p>Components build on stencils</p>
-                <h3>Calender:</h3>
-                <calendar-input></calendar-input>
+                <p>Components build on stencils Js</p>
+                <div class="col-4">
+                    <h4>Calender:</h4>
+                    <calendar-input></calendar-input>
+                </div>
             </div>
         )
     }
